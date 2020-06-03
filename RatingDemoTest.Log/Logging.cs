f325785af510746b36d36b2b7652a200a8e6a695
ndhieuwin10 <dinhhieu9095@gmail.com>
2020-06-04 03:20:49 +0700

    Logging

---
+++ RatingDemoTest.Log/Logging.cs
@@ -0,0 +1,51 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatingDemoTest.Log
+{
+    public class Logging
+    {
+        public static ILog Logger = LogManager.GetLogger(typeof(Logging));
+        /// <summary>
+        /// Ghi log Info
+        /// </summary>
+        /// <param name="message">Nội dung log</param>
+        public static void LogInfo(string message)
+        {
+            Logger.Info(message);
+        }
+
+        /// <summary>
+        /// Ghi log Info
+        /// </summary>
+        /// <param name="message">Nội dung log</param>
+        /// <param name="exception">Đối tượng Exception</param>
+        public static void LogInfo(string message, Exception exception)
+        {
+            Logger.Info(message, exception);
+        }
+
+        /// <summary>
+        /// Ghi log Error
+        /// </summary>
+        /// <param name="message">Nội dung log</param>
+        public static void LogError(string message)
+        {
+            Logger.Error(message);
+        }
+
+        /// <summary>
+        /// Ghi log Error
+        /// </summary>
+        /// <param name="message">Nội dung log</param>
+        /// <param name="exception">Đối tượng Exception</param>
+        public static void LogError(string message, Exception exception)
+        {
+            Logger.Error(message, exception);
+        }
+    }
+}
